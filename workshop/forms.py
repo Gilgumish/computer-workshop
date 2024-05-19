4c58a32b99205f2b4c7236fb6ff693441eecb0d3
Dmytro Pukas <pukasdima2002@gmail.com>
2024-05-19 14:14:33 +0200

    Fix navigation bar and button for masters

--- workshop/forms.py
+++ workshop/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from .models import Component, Computer, Master, User, Cart
+from .models import Component, Computer, Master, User
 
 
 class UserForm(forms.ModelForm):
