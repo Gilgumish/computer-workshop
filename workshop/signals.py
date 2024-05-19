79b1bcd416dd77fdd563927a66092ac528dba86e
Dmytro Pukas <pukasdima2002@gmail.com>
2024-05-19 21:30:23 +0200

    Deleted signals.py

--- workshop/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import User, Client
-
-
-@receiver(post_save, sender=User)
-def create_client_profile(sender, instance, created, **kwargs):
-    if created and instance.is_client:
-        Client.objects.create(user=instance)
